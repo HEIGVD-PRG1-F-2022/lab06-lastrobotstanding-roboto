8a9254fba86d7742d0da0e500f4723ca624a0421
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-12-06 17:53:57 +0100

    Format Roboto.cpp

--- robots/Roboto.cpp
+++ robots/Roboto.cpp
@@ -2,34 +2,27 @@
 #include <iostream>
 
 
-Roboto::Roboto(size_t width, size_t height, unsigned energy, unsigned power): width(width), height(height), energy(energy), power(power){
-
+Roboto::Roboto(size_t width, size_t height, unsigned energy, unsigned power) : width(width), height(height), energy(energy), power(power) {
 }
-void Roboto::setConfig(size_t w, size_t h, unsigned e, unsigned p){
+void Roboto::setConfig(size_t w, size_t h, unsigned e, unsigned p) {
     width = w;
     height = h;
     energy = e;
     power = p;
-
 }
-string Roboto::action(vector<string> updates){
-
+string Roboto::action(vector<string> updates) {
 }
 string Roboto::name() const {
     return "NomDeNotreRobot";
 }
-Case Roboto::readOnBoard(int relativeX, int relativeY){
-
+Case Roboto::readOnBoard(int relativeX, int relativeY) {
 }
 
 //private
 
-void Roboto::extractBoard(string update){
-
+void Roboto::extractBoard(string update) {
 }
-Damage Roboto::receiveDamage(string update){
-
+Damage Roboto::receiveDamage(string update) {
 }
-string Roboto::chooseAction(){
-
+string Roboto::chooseAction() {
 }
