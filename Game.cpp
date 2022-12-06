42190f5e0daef26302d6a71d10308931ca701781
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-12-06 15:04:02 +0100

    Start game loop of Game

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,5 @@
 #include "Game.h"
 #include "RobotState.h"
-#include "build/_deps/libdio-src/include/libdio/display.h"
 #include <libdio/display.h>
 #include <random>
 #include <string>
@@ -10,12 +9,11 @@ const int BASE_POWER = 1;
 
 //Function taken from this snippet https://stackoverflow.com/a/19728404
 int getRandomNumber(int min, int max) {
-    // std::random_device rd;                           // Only used once to initialise (seed) engine
-    // std::mt19937 rng(rd());                          // Random-number engine used (Mersenne-Twister in this case)
-    // std::uniform_int_distribution<int> uni(min, max);// Guaranteed unbiased
-    // auto random_integer = uni(rng);
-    // return random_integer;
-    return min + (rand() % static_cast<int>(max - min + 1));
+    std::random_device rd;                           // Only used once to initialise (seed) engine
+    std::mt19937 rng(rd());                          // Random-number engine used (Mersenne-Twister in this case)
+    std::uniform_int_distribution<int> uni(min, max);// Guaranteed unbiased
+    auto random_integer = uni(rng);
+    return random_integer;
 }
 
 Game::Game(unsigned nbRobots) : nbRobots(nbRobots) {
@@ -28,12 +26,29 @@ void Game::start() {
     d << "Welcome in LastRobotStanding, the fight has started...\n";
     d.print();
     generateRobots();
-    d << "robots setuped !!";
-    d.print();
+
     //game loop
-    // while (true) {
-    printBoard();
-    // }
+    string boardUpdate;
+    vector<string> updates;
+    vector<string> moves;
+    vector<string> attacks;
+    string action;
+    while (true) {
+        boardUpdate = "board " + computeBoardAsString();
+        for (RobotState state: robots) {
+            updates.push_back(boardUpdate);
+            action = state.askAction(updates);
+            updates.clear();
+        }
+
+        //Get and apply attacks
+        for (RobotState state: robots) {
+            if (state.getLastAction())
+        }
+
+        printBoard();
+        Display::clearScreen();
+    }
 }
 
 void Game::generateRobots() {
@@ -42,12 +57,10 @@ void Game::generateRobots() {
         randomX = getRandomNumber(0, (int) size - 1);
         randomY = getRandomNumber(0, (int) size - 1);
         robots.push_back(RobotState(randomX, randomY, BASE_ENERGY, BASE_POWER, size, size));
-        cout << "new robot" << randomX;
     }
 }
 
-void Game::printBoard() {
-    //Create an empty board
+vector<vector<string>> Game::buildDynamicBoard() {
     vector<vector<string>> board = vector<vector<string>>(size, vector<string>(size, " "));
 
     //For each RobotState we add them in the board with their number
@@ -57,6 +70,32 @@ void Game::printBoard() {
         cell = (cell != " ") ? "C" : to_string(index);//a C char is displayed when 2 robots (or more) are on the same cell
         index++;
     }
+    return board;
+}
 
-    Display::displayGrid(board);
+void Game::printBoard() {
+    //Create an empty board
+    vector<vector<string>> board = buildDynamicBoard();
+
+    for (auto line: board) {
+        for (auto cell: line) {
+            cout << cell;
+        }
+        cout << endl;
+    }
+    //TODO: refactor with displayGrid from libdio
+    // Display::DString h;
+    // h.displayGrid<string>(board, true);
+    // h.print();
+}
+
+string Game::computeBoardAsString() {
+    vector<vector<string>> board = buildDynamicBoard();
+    string msg;
+    for (auto line: board) {
+        for (auto cell: line) {
+            msg += cell;
+        }
+    }
+    return msg;
 }
--- Game.h
+++ Game.h
@@ -19,6 +19,8 @@ public:
     void start();
     void generateRobots();//fonction
     void printBoard();
+    vector<vector<string>> buildDynamicBoard();
+    string computeBoardAsString();
 };
 
 
