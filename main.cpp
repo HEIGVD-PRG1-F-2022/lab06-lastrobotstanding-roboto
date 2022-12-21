e41d0356465dae48454701f05a746cb7ea94cd19
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-12-21 01:49:05 +0100

    Add test mode in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,68 @@
 #include <libdio/input.h>
 
 #include <ctime>
+#include <iomanip>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-const int ROBOT_NUMBER = 2;
-int main() {
-    vector<RobotPack> robotPacks = {{"Roboto", 1}, {"RandomRoboto", 1}};
 
+const int ROBOT_NUMBER = 6;
+int main() {
+    //The list and number of robots we want to play with
+    const vector<RobotPack> robotPacks = {{"Roboto", 5}, {"BetterRoboto", 1}, {"RandomRoboto", 5}};
+
+    const bool TEST_MODE = true;                //the test mode runs TEST_ROUNDS_NUMBER rounds without display and show live stats
+    const unsigned int TEST_ROUNDS_NUMBER = 250;//number of rounds for the test mode
+
+    //Seed randomness and init console
     srand(time(0));
     Display::init();
-    Game game(ROBOT_NUMBER);
-    game.start(robotPacks);
+
+    //Run test mode or a single round
+    if (TEST_MODE) {
+        vector<string> winnersList;
+        map<string, unsigned> counters;
+
+        //Play the TEST_ROUNDS_NUMBER rounds and store winners
+        for (unsigned i = 1; i <= TEST_ROUNDS_NUMBER; i++) {
+            Game game(ROBOT_NUMBER);
+            string winner = game.start(robotPacks, false);
+            if (!winner.empty()) {
+                winnersList.push_back(winner);
+            }
+
+            //Calculate and display live statistics
+            counters.clear();
+            for (auto pack: robotPacks) {
+                counters.insert({pack.className, 0});//set counters to 0 for all robot classes
+            }
+            //Count the entire list
+            for (auto currentWinner: winnersList) {
+                counters.at(currentWinner)++;
+            }
+            //Order by descending wins count
+            vector<pair<string, unsigned>> orderedCounters = vector<pair<string, unsigned>>(counters.size(), pair<string, unsigned>());
+            copy(counters.begin(), counters.end(), orderedCounters.begin());
+            sort(orderedCounters.begin(), orderedCounters.end(), [](const pair<string, unsigned> &count1, const pair<string, unsigned> &count2) -> bool {
+                return count1.second > count2.second;
+            });
+            //Finally, display the stats
+            Display::DString stats(Display::Color::AQUA);
+            stats << "LIVE STATS - Round: " << i << "/" << TEST_ROUNDS_NUMBER << ":\n";
+            stats.setColor(Display::Color::ORANGE);
+            for (auto count: orderedCounters) {
+                stats << count.first << " " << count.second << " wins - " << (int) ((double) count.second / i * 100) << "% \n";
+            }
+            Display::clearScreen();
+            stats.print();
+        }
+    } else {
+        //Just turn once
+        Game game(ROBOT_NUMBER);
+        game.start(robotPacks);
+    }
 }
